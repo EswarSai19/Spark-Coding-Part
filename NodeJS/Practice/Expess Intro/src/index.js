4a4d3fd551d110dd378d7c8902e5bea1d22d862a
EswarSai19 <eswarsai219@gmail.com>
2023-01-23 07:08:55 +0530

    Little changes

--- NodeJS/Practice/Expess Intro/src/index.js
+++ NodeJS/Practice/Expess Intro/src/index.js
@@ -9,14 +9,34 @@ app.get('/', (req,res)=>{
     res.send("Hey there, I m back")
 })
 
-app.get('/posts', (req,res)=>{
-    res.send(db.posts)
-})
+// app.get('/posts', (req,res)=>{
+//     res.send(db.posts)
+// })
 
 app.get('/posts/:id', (req,res)=>{
     let id = +req.params.id
     let posts = db.posts.find((el)=>el.id===id)
+    if(posts) res.send(posts)
+    else res.send(`Person with ID : ${id} Not found`)
+    
 })
+
+app.get('/posts', (req,res)=>{
+    let {name,msg} = req.query
+    let posts = db.posts
+    if(name){
+       posts =  posts.filter((el)=> el.name=== name)
+        res.send(posts)  
+    }
+    if(msg){
+        posts = posts.filter((el)=> el.Message.includes(msg))
+        res.send(posts)  
+    }           
+     res.send(posts)
+    
+})
+
+
 
 app.post('/posts',(req,res)=>{
     db.posts.push({"id":4, "Message":"Message-4"})
