ac9212fb818f2d786c7c73218ea21404278b7635
EswarSai19 <eswarsai219@gmail.com>
2023-04-11 21:27:24 +0530

    Only small changes done

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -8,12 +8,29 @@
 //     console.log(generateRandomNumber());
 
 
-    var unixts = 1676618685488;
-var date = new Date(unixts);
 
+
+// date converting
+
+/*
+ var unixts = 1676618685488;
+var date = new Date(unixts);
 var fdate = date.getFullYear() + '/' + ("0" + (date.getMonth() + 1)).slice(-2) + '/' + ("0" + date.getDate()).slice(-2);
 console.log(fdate);
 
 
 let myDate = new Date()
-console.log(Date.now()/(1000*60*60*24))
+let shortDate = Math.floor(Date.now()/1000)
+shortDate = Math.floor(shortDate/(60*24*60))
+console.log(Date.now(0).setSeconds(shortDate))
+ */
+
+
+
+// function truncate(source, size) {
+//     return source.length > size ? source.slice(0, size - 1) + "…" : source;
+//   }
+  
+//   var res = truncate('Truncate text to fit in 3 lines ', 90);
+//   console.log(res);
+
